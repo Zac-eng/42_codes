dcc83d1ad709b6f3900b6ad4a4b29e76cdcc3fa2
Zac-eng <zack.abten@gmail.com>
2024-10-05 22:54:05 +0900

    trying to enable mariadb with TCP connection

--- lv5/inception/srcs/requirements/mariadb/init.sql
+++ lv5/inception/srcs/requirements/mariadb/init.sql
@@ -1,3 +1,10 @@
 CREATE DATABASE wordpress_db DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-CREATE USER 'wordpress_user'@'localhost' IDENTIFIED BY 'strong_password';
-GRANT ALL PRIVILEGES ON wordpress_db.* TO 'wordpress_user'@'localhost';
+CREATE USER 'boss'@'192.168.65.1' IDENTIFIED BY 'b0ss_passW0rd';
+GRANT ALL PRIVILEGES ON wordpress_db.* TO 'boss'@'192.168.65.1';
+CREATE USER 'hmiyazak'@'192.168.65.1' IDENTIFIED BY 'harutoM1yazak1';
+GRANT ALL PRIVILEGES ON wordpress_db.* TO 'hmiyazak'@'192.168.65.1';
+CREATE USER 'boss'@'127.0.0.1' IDENTIFIED BY 'b0ss_passW0rd';
+GRANT ALL PRIVILEGES ON wordpress_db.* TO 'boss'@'127.0.0.1';
+CREATE USER 'hmiyazak'@'127.0.0.1' IDENTIFIED BY 'harutoM1yazak1';
+GRANT ALL PRIVILEGES ON wordpress_db.* TO 'hmiyazak'@'127.0.0.1';
+FLUSH PRIVILEGES;
