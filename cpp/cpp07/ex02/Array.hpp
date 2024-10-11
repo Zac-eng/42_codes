6f021dc6eef66a82637f950d9e94d77564b5a455
Haruto Miyazaki <hmiyazak@c1r3s8.42tokyo.jp>
2024-10-11 11:41:01 +0900

    done ex02

--- cpp/cpp07/ex02/Array.hpp
+++ cpp/cpp07/ex02/Array.hpp
@@ -1,19 +1,30 @@
 #ifndef ARRAY_HPP
 # define ARRAY_HPP
 
-template <typename >
+# include <string>
+# include <iostream>
+# include <cstdlib>
+# include <exception>
+
+template <typename T>
 class Array {
 
 private:
+  T* _elements;
+  std::size_t _size;
 
 public:
   Array(void);
   Array(unsigned int n);
+  Array(const Array& object);
   ~Array();
   Array& operator = (const Array& object);
-  std::size_t size(void) const ;
-
+  T& operator [] (int index);
+  T operator [] (int index) const;
+  std::size_t size(void) const;
 
 };
+
+# include "Array.tpp"
 
 #endif
