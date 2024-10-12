8e629549b3f9ccb616e3fe371af16641d82e9b2a
Zac-eng <zack.abten@gmail.com>
2024-10-12 20:40:46 +0900

    enable auto installation of database

--- lv5/inception/srcs/requirements/mariadb/init.sql
+++ lv5/inception/srcs/requirements/mariadb/init.sql
@@ -1,10 +1,6 @@
 CREATE DATABASE wordpress_db DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-CREATE USER 'boss'@'wordpress' IDENTIFIED BY 'b0ss_passW0rd';
-GRANT ALL PRIVILEGES ON wordpress_db.* TO 'boss'@'wordpress';
-CREATE USER 'hmiyazak'@'wordpress' IDENTIFIED BY 'harutoM1yazak1';
-GRANT ALL PRIVILEGES ON wordpress_db.* TO 'hmiyazak'@'wordpress';
-CREATE USER 'boss'@'127.0.0.1' IDENTIFIED BY 'b0ss_passW0rd';
-GRANT ALL PRIVILEGES ON wordpress_db.* TO 'boss'@'127.0.0.1';
-CREATE USER 'hmiyazak'@'127.0.0.1' IDENTIFIED BY 'harutoM1yazak1';
-GRANT ALL PRIVILEGES ON wordpress_db.* TO 'hmiyazak'@'127.0.0.1';
+CREATE USER 'boss'@'%' IDENTIFIED BY 'b0ss_passW0rd';
+GRANT ALL PRIVILEGES ON wordpress_db.* TO 'boss'@'%';
+CREATE USER 'hmiyazak'@'%' IDENTIFIED BY 'harutoM1yazak1';
+GRANT ALL PRIVILEGES ON wordpress_db.* TO 'hmiyazak'@'%';
 FLUSH PRIVILEGES;
