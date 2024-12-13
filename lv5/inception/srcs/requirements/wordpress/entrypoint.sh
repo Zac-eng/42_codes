9294621303137d67bbc70e60e35a95b4cbc45ea2
Zac-eng <zack.abten@gmail.com>
2024-12-13 12:57:56 +0900

    done setting variables with wp cli

--- lv5/inception/srcs/requirements/wordpress/entrypoint.sh
+++ lv5/inception/srcs/requirements/wordpress/entrypoint.sh
@@ -4,19 +4,23 @@
 mkdir -p /var/www/html
 cd /var/www/html
 
+#fetching wordpress files
 wget https://wordpress.org/latest.tar.gz && \
     tar -xzvf latest.tar.gz &&\
     mv ./wordpress/* /var/www/html && \
     rm -rf latest.tar.gz wordpress && \
     apt clean && rm -rf /var/lib/apt/lists/*
 
+#runnning wordpress with the user www-data
 chown -R www-data:www-data /var/www/html/ && \
 chmod -R 755 /var/www/html/
 
+#installing wp command
 wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
 
+#setting database_name, user_name, user_password
 useradd -m env_setter
 sudo -u env_setter -i -- \
 wp config create \
