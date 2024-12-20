c3c727934eddc2508d7b57e2c8dc1007a9525cf3
Zac-eng <zack.abten@gmail.com>
2024-12-20 22:25:02 +0900

    small, but needed to add /g to sed command

--- lv5/inception/srcs/requirements/nginx/entrypoint.sh
+++ lv5/inception/srcs/requirements/nginx/entrypoint.sh
@@ -4,6 +4,6 @@
 set -e
 
 sed -i "s/DOMAIN_NAME/$DOMAIN_NAME/g" /etc/nginx/conf.d/default.conf
-sed -i "s/CERTS_/${CERTS_}" /etc/nginx/conf.d/default.conf
+sed -i "s/CERTS_/${CERTS_}/g" /etc/nginx/conf.d/default.conf
 
 exec "$@"
