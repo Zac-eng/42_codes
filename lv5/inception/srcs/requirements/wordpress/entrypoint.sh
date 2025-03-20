205ec9c7753ad35efd8cb2d14bbaae823401f0a8
Zac-eng <zack.abten@gmail.com>
2025-03-20 11:36:56 +0900

    done adding and setting users

--- lv5/inception/srcs/requirements/wordpress/entrypoint.sh
+++ lv5/inception/srcs/requirements/wordpress/entrypoint.sh
@@ -22,13 +22,28 @@ mv wp-cli.phar /usr/local/bin/wp
 
 #setting database_name, user_name, user_password
 useradd -m env_setter
+
 sudo -u env_setter -i -- \
 wp config create \
     --path=/var/www/html \
     --dbname=${MYSQL_DATABASE} \
     --dbuser=${MYSQL_USER} \
     --dbhost=mariadb \
-    --dbpass=${MYSQL_PASSWORD}
+    --dbpass=${MYSQL_PASS}
+
+sudo -u env_setter -i -- \
+wp core install \
+    --path=/var/www/html \
+    --url="https://${DOMAIN_NAME}" \
+    --title="${WP_TITLE}" \
+    --admin_user="${WP_ADMIN}" \
+    --admin_password="${WP_ADMIN_PASS}" \
+    --admin_email="${WP_ADMIN_EMAIL}"
+
+sudo -u env_setter -i -- \
+wp user create ${WP_USER} ${WP_USER_EMAIL} \
+    --path=/var/www/html \
+    --user_pass=${WP_USER_PASS}
 
 mkdir -p /run/php
 
